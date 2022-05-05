8a528e4c5c8f132d4ca7f54ad1ccfaec9408be14
brunoleo223 <brunoleo223@gmail.com>
2022-05-05 09:40:04 -0300

    form to capture lead

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,8 +99,32 @@ function updateResultData(data){
     const title = document.querySelector('.resultado .conteudo h2');
     const description = document.querySelector('.resultado .conteudo p');
     const img = document.querySelector('.resultado .conteudo img');
-
+    
     title.textContent = data.title;
     description.textContent = data.description;
     img.setAttribute('src', data.img);
 }
+
+
+
+// Validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+    
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+    
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
